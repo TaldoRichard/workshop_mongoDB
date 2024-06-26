0bdb8924662d0e64310436160c585306b3edf918
TaldoRichard <richarddegodois@gmail.com>
2024-06-26 20:04:16 -0300

    Operação de instanciação da base de dados

--- src/main/java/com/example/workshopmongo/domain/User.java
+++ src/main/java/com/example/workshopmongo/domain/User.java
@@ -10,17 +10,17 @@ import org.springframework.data.mongodb.core.mapping.Document;
 public class User implements Serializable{
 	private static final long serialVersionUID = 1L;
 	
-	private String name;
 	@Id
 	private String id;
+	private String name;
 	private String email;
 	
 	public User() {
 	}
 
-	public User(String name, String id, String email) {
-		this.name = name;
+	public User(String id, String name, String email) {
 		this.id = id;
+		this.name = name;
 		this.email = email;
 	}
 
